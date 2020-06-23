06c769018053d19734c629da68eca7c93c891c14
Robin Coma Delperier <robin@placeme.io>
2020-06-23 14:30:46 +0200

    feat(Animal): interface

--- src/app/animal/animal-preview/animal-preview.component.ts
+++ src/app/animal/animal-preview/animal-preview.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from '../shared/animal';
+
 @Component({
   selector: 'app-animal-preview',
   templateUrl: './animal-preview.component.html',
-  styleUrls: ['./animal-preview.component.scss']
+  styleUrls: ['./animal-preview.component.scss'],
 })
 export class AnimalPreviewComponent implements OnInit {
+  animal: Animal;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.animal = {
+      name: 'Felix',
+      espece: 'Chat',
+      veterinaire: 'Mr Smith',
+      commentaire: 'Super sympas ce chat !',
+    };
   }
-
 }
---
+++ src/app/animal/shared/animal.ts
@@ -0,0 +1,6 @@
+export interface Animal {
+  name: string;
+  espece: string;
+  veterinaire: string;
+  commentaire: string;
+}
