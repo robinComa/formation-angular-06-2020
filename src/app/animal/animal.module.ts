bfcc9776523657bc32fb477d10d221aacfddd7b8
Robin Coma Delperier <robin@placeme.io>
2020-06-23 16:30:58 +0200

    feat(Directive): text transform uppercase

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AnimalPreviewComponent } from './animal-preview/animal-preview.component';
 import { AnimalService } from './shared/animal.service';
 import { AnimalListComponent } from './animal-list/animal-list.component';
+import { UppercaseDirective } from './shared/uppercase.directive';
 
 @NgModule({
-  declarations: [AnimalPreviewComponent, AnimalListComponent],
+  declarations: [AnimalPreviewComponent, AnimalListComponent, UppercaseDirective],
   imports: [CommonModule],
   exports: [AnimalPreviewComponent, AnimalListComponent],
   providers: [AnimalService],
