8d600ec2e462e50453740bfc89493c37a73cfbbe
Robin Coma Delperier <robin@placeme.io>
2020-06-26 11:20:56 +0200

    feat(Tests): resolver testing with mocked ActivatedRouteSnapshot and HttpTestingController asserts

--- src/app/animal/shared/animal-resolver.service.spec.ts
+++ src/app/animal/shared/animal-resolver.service.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AnimalResolverService } from './animal-resolver.service';
 import { AnimalService } from './animal.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { Animal } from './animal';
+import { environment } from 'src/environments/environment';
 
 fdescribe('AnimalResolverService', () => {
   let service: AnimalResolverService;
+  let httpTestCtlr: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +20,34 @@ fdescribe('AnimalResolverService', () => {
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(AnimalResolverService);
+    httpTestCtlr = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should do a http request', () => {
+    const id = 1;
+    const route = new ActivatedRouteSnapshot();
+    route.params = { id };
+    service.resolve(route).subscribe();
+
+    const request = httpTestCtlr.expectOne(
+      `${environment.endpoint}/animals/${id}`
+    );
+    expect(request.request.method).toEqual('GET');
+
+    request.flush({
+      id,
+      name: '',
+      espece: '',
+      naissance: 1234567890123,
+      picture: '',
+      veterinaire: '',
+      commentaire: '',
+    } as Animal);
+
+    httpTestCtlr.verify();
   });
 });
--- src/app/animal/shared/animal-resolver.service.ts
+++ src/app/animal/shared/animal-resolver.service.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
-import {
-  Resolve,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-} from '@angular/router';
-import { Animal } from './animal';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { Animal } from './animal';
 import { AnimalService } from './animal.service';
 
 @Injectable()
 export class AnimalResolverService implements Resolve<Animal> {
   constructor(private animalService: AnimalService) {}
 
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Animal | Observable<Animal> | Promise<Animal> {
+  resolve(route: ActivatedRouteSnapshot): Observable<Animal> {
     const id = +route.paramMap.get('id');
     return this.animalService.get(id);
   }
