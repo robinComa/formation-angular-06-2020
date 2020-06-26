e6a3c8419949d088b7b31abc1c35d2e585521e59
Robin Coma Delperier <robin@placeme.io>
2020-06-26 14:46:02 +0200

    feat(Veterinaire): form

--- src/app/veterinaire/veterinaire.module.ts
+++ src/app/veterinaire/veterinaire.module.ts
@@ -5,6 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { VeterinaireRoutingModule } from './veterinaire-routing.module';
 import { VeterinaireListComponent } from './veterinaire-list/veterinaire-list.component';
@@ -22,6 +24,8 @@ import { VeterinaireFormComponent } from './veterinaire-form/veterinaire-form.co
     MatButtonModule,
     MatListModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [VeterinaireService],
 })
